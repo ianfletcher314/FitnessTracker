a3551dae7a8d51c2bd92a27a45457c1bfa2cf142
Ian Fletcher <ianfletcher314@gmail.com>
2021-05-28 15:04:09 -0500

    controlers index.js file built need to make routs now that db is seeding

---
+++ controllers/index.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const db = require("../models");
+
+router.get("/exercise", (req, res) => {
+    // res.sendFile()
+});
+router.get("/stats", (req, res) => {
+ 
+});
+router.get("/workouts", (req,res) =>{
+
+});
+
+
+
+module.exports = router;
