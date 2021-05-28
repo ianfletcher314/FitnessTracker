7c07df7b67add887cd2c0c89ac762a2a72dba384
Ian Fletcher <ianfletcher314@gmail.com>
2021-05-28 11:29:34 -0500

    schema/model of exercise made and resistance built to extend it. I got the extention code from an npm package so I am  unsure if it works. I need to build out cardio and then make sure all of he models are exporting and importing correctly before testing.

---
+++ models/index.js
@@ -0,0 +1,75 @@
+// CONVERT THIS TO CODE THAT WORKS FOR US 
+const mongoose = require("mongoose");
+
+
+const Schema = mongoose.Schema;
+
+function extendSchema (Schema, definition, options) {
+    return new mongoose.Schema(
+      Object.assign({}, Schema.obj, definition),
+      options
+    );
+  }
+
+
+const exercise = new Schema({
+  type: {
+    type: String,
+    trim: true,
+    required: "Type is Required"
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: "Name is Required"
+  },
+
+  duration: {
+    type: Number,
+    required: true
+  },
+
+  date: {
+    type: Date,
+    default: Date.now
+  },
+
+  lastUpdated: Date,
+});
+// convert this to give full amount of weight they have lifted
+// UserSchema.methods.setFullName = function() {
+//   this.fullName = `${this.firstName} ${this.lastName}`;
+
+//   return this.fullName;
+// };
+
+const resistance = extendSchema(exercise, {
+    weight: {
+        type: Number,
+        required: true
+      },
+      reps: {
+        type: Number,
+        required: true
+      },
+    
+      sets: {
+        type: Number,
+        required: true
+      },
+
+  });
+
+
+
+UserSchema.methods.lastUpdatedDate = function() {
+  this.lastUpdated = Date.now();
+
+  return this.lastUpdated;
+};
+
+const exerciseModel = mongoose.model("Exercise", exercise);
+const resistanceModel = mongoose.model('Resistance', resistance);
+
+module.exports = exerciseModel;
+module.exports = resistanceModel;
