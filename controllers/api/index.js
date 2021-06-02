e92a9affd28dd587eebee169461ff03f8acfec4c
Ian Fletcher <ianfletcher314@gmail.com>
2021-06-02 18:44:33 -0500

    posting to db but without data still

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,33 +4,37 @@ const path = require('path')
 
 router.get("/workouts", (req, res) => {
     db.Workout.find({})
-      .then(data => {
-        res.json(data);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
 });
 router.post("/workouts", (req, res) => {
-    db.Workout.create({req})
-      .then(data => {
+    db.Workout.create({ req })
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+router.put("/workouts/:id", ({body,params}, res) => {
+    // const request = req.params
+    // // const filter = { id: request };
+    // // const update = { req };
+    // console.log(req,"requeststring")
+
+    db.Workout.findOneAndUpdate({_id:params.id},{$push: {exercise:body}}, {
+      new: true
+    })
+    .then(data => {
         res.json(data);
       })
       .catch(err => {
         res.status(400).json(err);
       });
-});
-router.put("/workouts/:id", (req, res) => {
-    const request = req.params
-    // const filter = { id: 'Jean-Luc Picard' };
-    // const update = { age: 59 };
-    console.log(request)
-    
-    // `doc` is the document _after_ `update` was applied because of
-    // `new: true`
-    // let doc = await Character.findOneAndUpdate(filter, update, {
-    //   new: true
-    // });
 
 })
 
@@ -39,5 +43,4 @@ router.put("/workouts/:id", (req, res) => {
 
 
 
-  module.exports = router;
-
+module.exports = router;
